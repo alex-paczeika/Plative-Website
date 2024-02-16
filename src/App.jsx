baebed33893fcf8674a48f7deb594004b8dd72e4
Alex Paczeika <alex.paczeika@gmail.com>
2024-02-16 23:50:23 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,21 @@ import People from './components/People/People'
 import Portfolio from './components/Portfolio/Portfolio'
 import Work from './components/Work/Work'
 import css from './styles/App.module.scss'
+import SmartBanner from 'react-smartbanner';
 const App = () => {
   //don't forget to add font link in index.html
   return (
     <div className={`bg-primary ${css.container}`}>
+
+  <SmartBanner
+    title={'Facebook'} // Your app title
+    author={'Facebook, Inc.'} // Your app author
+    force={'ios'} // Force iOS banner
+    appStoreLanguage={'us'} // App Store language
+    // Other props as needed
+  />
+
+
       <Header/>
       <Hero/>
 
